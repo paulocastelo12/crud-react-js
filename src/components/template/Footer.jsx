d8b5b1a96f88321aa51616d5c51d257a77c2f1c3
PAULO CASTELO <paulo@amazongas.com.br>
2021-04-10 15:34:08 -0400

    first-commit

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+export default (props) =>{
+    return (
+        <footer className="footer">
+            <span>
+                Desnvolvido com <i className="fa fa-heart text-danger"></i> por  <strong>Paulo R. Castelo</strong>
+            </span>
+        </footer>
+    )
+}
